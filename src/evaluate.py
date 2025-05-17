49dff1733c60b4b1686003ebc2d0f41aee09fc40
umit akdeniz <umitakdenizjoy@gmail.com>
2025-05-17 23:32:50 +0300

    Update evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,10 +1,10 @@
+# src/evaluate.py
+
 import yaml
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import roc_curve, auc
-from keras.models import load_model
 from sklearn.preprocessing import StandardScaler
-
 from src.data_loader import get_features_labels
 from src.model import build_residual_model
 
@@ -16,14 +16,17 @@ def evaluate_from_config(config_path="configs/config.yaml"):
     scaler = StandardScaler()
     X_test = scaler.fit_transform(X_test)
 
-    model = build_residual_model(input_dim=X_test.shape[1], output_dim=y_test.shape[1])
+    model = build_residual_model(
+        input_dim=X_test.shape[1],
+        output_dim=y_test.shape[1]
+    )
     model.load_weights("outputs/keras_model_best.h5")
 
     y_pred = model.predict(X_test)
     fpr, tpr, _ = roc_curve(y_test[:,1], y_pred[:,1])
     roc_auc = auc(fpr, tpr)
 
-    print(f"AUC Score: {roc_auc:.4f}")
+    print(f"AUC: {roc_auc:.4f}")
 
     plt.figure()
     plt.plot(tpr, fpr, label=f"AUC = {roc_auc:.2%}")
