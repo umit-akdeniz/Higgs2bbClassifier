79a1622a3385264c5e3e5798a1b0c00e079ef4f2
umit akdeniz <umitakdenizjoy@gmail.com>
2025-05-17 23:46:04 +0300

    Update model.py

--- src/model.py
+++ src/model.py
@@ -6,20 +6,27 @@ def build_residual_model(input_dim, output_dim, dropout_rate=0.3):
     x = BatchNormalization()(inputs)
     x = Dense(128, activation='relu')(x)
     x = Dropout(dropout_rate)(x)
+
+    # Residual block 1
     res = Dense(128, activation='relu')(x)
     res = Dropout(dropout_rate)(res)
     x = Add()([x, res])
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
+
+    # Residual block 2
     res = Dense(128, activation='relu')(x)
     res = Dropout(dropout_rate)(res)
     x = Add()([x, res])
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
+
+    # Final layers
     x = Dense(64, activation='relu')(x)
     x = Dropout(dropout_rate)(x)
     x = Dense(32, activation='relu')(x)
     x = Dropout(dropout_rate)(x)
+
     outputs = Dense(output_dim, activation='softmax')(x)
     model = Model(inputs=inputs, outputs=outputs)
     return model
